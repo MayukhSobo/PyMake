5251505ad76c0521907cf090735594240c61a55a
Mayukh Sarkar <mayukh2012@hotmail.com>
2016-05-23 00:01:21 +0530

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 import sys
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 import re
+import string
 
 
 class Grammar(object):
@@ -26,12 +27,20 @@ class Grammar(object):
     def __init__(self, automateFile=None):
         with open(automateFile, 'r') as af:
             self.data = af.read()
-        self.mTags = ['proto', 'workspace', 'project']
-        self.oTags = ['run_test', 'summary']
-        self.rmTag = ['codeline']
-        self.uTags = self.uTags = self.mTags + self.oTags + self.rmTag
-        self.aTags = self.uTags
+        self.mTags = [{'tag': 'proto', 'typed': 'mono1'}, {'tag': 'workspace', 'typed': 'null'}, {'tag': 'project', 'typed': 'null'}]
+        self.oTags = [{'tag': 'run_test', 'typed': 'multi'}, {'tag': 'summary', 'typed': 'mono2'}]
+        self.rmTag = [{'tag': 'codeline', 'typed': 'null'}]
+        self.aTags = self.mTags + self.oTags + self.rmTag
+        self.allData = {
+        			'proto': [],
+        			'codeLine': None,
+        			'workspace': None,
+        			'project': None,
+        			'summary': [],
+        }
+        # self.aTags = self.uTags
         self._verify_syntax()
+        # self._final_checks()
 
     def _braced(self):
         """
@@ -79,26 +88,48 @@ class Grammar(object):
         :exception: Several instances of SyntaxError
         :return: None
         """
-        for each in self.uTags:
-            regex = each
+
+        # Check if all the mandatory tags are present
+        for each in self.mTags:
+            regex = each['tag']
             count = len(re.findall(regex, self.data.lower()))
             try:
                 if count < 1:
-                    e = "Can not find %s" % str(each).upper()
-                    raise SyntaxError(e)
-                elif count > 1:
-                    e = "Multiple entries of %s" % str(each).upper()
+                    e = "Can not find %s" % str(each['tag']).upper()
                     raise SyntaxError(e)
             except SyntaxError as what:
                 err = "SyntaxError: %s" % str(what)
                 print(err, file=sys.stderr)
                 sys.exit()
 
+        # Check if all tags are not present more than once
+        for each in self.aTags:
+            regex = each['tag']
+            count = len(re.findall(regex, self.data.lower()))
+            try:
+                if count > 1:
+                    e = "Multiple entries of %s" % str(each['tag']).upper()
+                    raise SyntaxError(e)
+            except SyntaxError as what:
+                err = "SyntaxError: %s" % str(what)
+                print(err, file=sys.stderr)
+                sys.exit()
+
+        # Check the relationally mandatory tag is present
+        try:
+        	if re.findall(r'RUN_TEST', self.data, re.IGNORECASE) and not re.findall(r'codeline', self.data, re.IGNORECASE):
+        		e = "Can not find CODELINE"
+        		raise SyntaxError(e)
+        except SyntaxError as what:
+            err = "SyntaxError: %s" % str(what)
+            print(err, file=sys.stderr)
+            sys.exit()
+
     def __core__(self, tag, typed):
         """
         Method to handle low level
         line ending of the .automate file
-        and also returns self.entries which
+        and also returns 'entries' which
         is used for some further operations
 
         It takes an argument called "typed" which
@@ -109,25 +140,109 @@ class Grammar(object):
 
         :CAUTION: Very low level magic method
                   and should not be used even
-                  in the Derived class
+                  in the Derived class. Do not
+                  change this method
 
         :param tag:  str
         :param typed: str (null, mono, multi)
 
-        :return: str (self.entries)
+        :return: str (entries)
         """
         entries = None
 
         if typed == "null":
             pass
-        elif typed == "mono":
+        elif typed == "mono1" or typed == "mono2":
             regex = tag + r"\s*\{(?:\n*\s*(.*?))\}"
-            entries = re.findall(regex, self.data, re.DOTALL | re.IGNORECASE)
-            print(entries)
+            match = re.findall(regex, self.data, re.DOTALL | re.IGNORECASE)
+            entries = match[0].split()
+            for i in xrange(len(entries)):
+				try:
+					# if not entries[i].endswith(r",") and entries[i + 1] == r",":
+					# 	entries.pop(i + 1)
+					# 	entries[i] += r","
+					# if not entries[i].endswith(r":") and entries[i + 1] == r":":
+					# 	entries.pop(i + 1)
+					# 	entries[i] += r":"
+					if entries[i][-1] not in string.punctuation and entries[i + 1][-1] in string.punctuation:
+						t = entries.pop(i + 1)
+						entries[i] += t
+				except IndexError:
+					pass
         elif typed == "multi":
             pass
         return entries
 
+    def __is_empty(self, entries, tag):
+    	"""
+    	Check if the tag passed does
+    	have some values in the entry.
+    	If the 'entries' is empty for the 'tag'
+    	it will throw an exception
+
+		:param entries: str (entries for each tag in .automate file)
+    	:param tag: str (any possible tag in .automate file)
+
+    	:CAUTION: Low level api and not to be used by derived classes
+
+    	:return: None
+    	"""
+    	try:
+    		if not entries:
+    			e = "No entries in %s" % str(tag).upper()
+    			raise AttributeError(e)
+    	except AttributeError as what:
+    		err = "AttributeError: %s" % str(what)
+    		print(err, file=sys.stderr)
+    		sys.exit()
+
+    def __delimit__(self, entries, delimit, tag):
+    	"""
+    	Fallback magic method for _delimit() method.
+
+    	It checks if all the 'entries' for a 'tag' 
+    	ends with the 'delimit' mentioned
+
+		:param tag: str ('tag' for which entries needed to be found)
+    	:param entries: list (entries for the 'tag' in .automate file)
+    	:param delimit: str (delimit that should be verified)
+
+    	:exception: SyntaxError() if each values in the 'entries'
+    							  doesn't end with the 'delimit'
+    	"""
+    	try:
+    		for each in entries:
+    			if not each.endswith(delimit):
+    				if each[-1].isalpha() or each[-1].isdigit():
+    					e = "Expected %s after %s" % (str(delimit), str(each))
+    				else:
+    					e = "Expected %s instead of %s in %s" % (str(delimit), str(each)[-1], str(each)[0:-1])
+    				raise SyntaxError(e)
+    	except SyntaxError as what:
+    		err = "SyntaxError: %s" % str(what)
+    		print(err, file=sys.stderr)
+    		sys.exit()
+    	else:
+    		print([each.split()[0].split(delimit)[0] for each in entries])
+
+    def _delimit(self):
+    	for each in self.aTags:
+    		entries = self.__core__(each['tag'], each['typed'])
+    		if each['typed'] == 'mono1' or each['typed'] == 'mono2':
+    			self.__is_empty(entries, each['tag'])
+
+    		if each['typed'] == 'mono1':
+    			# Because all the mono1 tags are ended with ','
+	    		self.__delimit__(entries, r",", each['tag'])
+	    	elif each['typed'] == 'mono2':
+	    		# Because all the mono2 tags are ended with ':' and  ',' both
+	    		self.__delimit__([entries[i] for i in xrange(len(entries)) if i % 2 != 0], r",", each['tag'])
+	    		self.__delimit__([entries[i] for i in xrange(len(entries)) if i % 2 == 0], r":", each['tag'])
+	    		# print([entries[i] for i in xrange(len(entries)) if i % 2 != 0])
+	    		# print([entries[i] for i in xrange(len(entries)) if i % 2 == 0])
+
+	def _final_checks(self):
+		pass
 
     def _verify_syntax(self, check_list="All"):
         """
@@ -148,6 +263,8 @@ class Grammar(object):
         if check_list.lower() == "all":
             self._braced()
             self._unique()
+            self._delimit()
+
 
 class Resolver(Grammar):
     def __init__(self, automateFile=None):
@@ -155,4 +272,3 @@ class Resolver(Grammar):
 
 if __name__ == '__main__':
     rs = Resolver(r"sample.txt")
-
