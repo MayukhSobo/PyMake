e2d0461b50f355845fe6e11ffb8e3e9c2dd1ac91
Mayukh Sarkar <mayukh2012@hotmail.com>
2016-05-25 18:30:51 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,167 +1,197 @@
-# importing python 3 print function in python 2
-from __future__ import print_function
-import re
-import sys
 
-data = """
-headerFiles {
-	hello.h,
-	hi.h,
-}
-
-sourceFiles {
-	hello.c,
-	hi.c,
-	main.cpp,
-}
-
- """
-
-# data = "Hello World!"
+    __author__ = 'Mayukh Sarkar'
+__email__ = 'mayukh.sarkar@morpho.com'
+import os
+import subprocess
+from abc import ABCMeta, abstractmethod
 
 
-# def prune(tag=None, desc=None):
-# 	importanceValues = ["mandatory", "optional"]
-# 	if desc is None:
-# 		desc = {}
-# 		desc['importance'] = "mandatory"
-# 		desc['levels'] = 1
-# 	elif r'importance' not in desc:
-# 		desc['importance'] = "mandatory"
-# 	elif r'levels' not in desc:
-# 		desc['levels'] = 1
-# 	else:
-# 		try:
-# 			if desc['importance'] not in importanceValues:
-# 				raise AttributeError("Unsupported IMPORTANCE value")
-# 				sys.exit()
-# 			if desc['levels'] < 0:
-# 				raise ValueError("Can not prune any negative levels")
-# 		except KeyError:
-# 			raise AttributeError("Unsupported pruning option")
-# 			sys.exit()
-# 	if tag is None:
-# 		raise ValueError("No tag value passed")
-# 	###########  Start checking the grammar ###########
+################# Unit testing ######################
+#### This should be uncommented for unit testing ####
+products = [
+    {"Product": "S65-85OS04_M2M_GP211_JC222_R6",
+     "PlatformName": "winPc",
+     "PlatformVariant": "eeprom",
+     "DocGeneration": False,
+     "BuildStatus": "Pass",
+     },
+]
+#####################################################
 
-# 	### Validation for mandatory tags ###
-# 	if desc['importance'] == 'mandatory':
-# 		regex = tag
-# 		count = len(re.findall(regex, data, re.IGNORECASE))
-# 		try:
-# 			if count < 1:
-# 				raise SyntaxError("Source Files are not given")
-# 			elif count > 1:
-# 				raise SyntaxError("Multiple entries of sourcefiles")
-# 		except SyntaxError as what:
-# 			err = "SyntaxError: %s" % str(what)
-# 			print(err, file=sys.stderr)
-# 			sys.exit()
-# 	#####################################
+class JCSIM(object):
+    __metaclass__ = ABCMeta
 
-# prune(tag=r'sourcefiles')
+    counter = 0
 
-############# Checking for unique tag occurances ############
-regex = r'sourceFiles'
-count = len(re.findall(regex, data))
-try:
-	if count < 1:
-		raise SyntaxError("Source Files are not given")
-	elif count > 1:
-		raise SyntaxError("Multiple entries of sourcefiles")
-except SyntaxError as what:
-	err = "SyntaxError: %s" % str(what)
-	print(err, file=sys.stderr)
-	sys.exit()
-#############################################################
+    def __init__(self, ws):
+        os.system("taskkill /f /im JCSim3x.exe")
+        self.jcsim = os.path.join(ws, r'tools', r'Jacade', r'SimulationEnvironment', r'JCSim3x.exe')
 
-############ Checking for brace mismatching  ###########
-try:
-	braceCount = 0
-	# tagData = ""
-	for each in data:
-		# tagData += each
-		if each == r"{":
-			# tag = re.match(r'.*?(\w+)\s*\{', tagData, re.DOTALL).group(1)
-			# tagData = ""
-			braceCount += 1
-		elif each == r"}":
-			braceCount -= 1
-		if braceCount > 1 or braceCount < 0:
-			if braceCount > 1:
-				e = "Expected matching }"
-			else:
-				e = "Expected matching {"
-			raise SyntaxError(e)
-	if braceCount == 1:
-		e = r"Expected matching }"
-		raise SyntaxError(e)
-except SyntaxError as what:
-	err = "SyntaxError: %s" % str(what)
-	print(err, file=sys.stderr)
-	sys.exit()
-########################################################
+    @abstractmethod
+    def _initiate_jcsim(self, proto=None):
+        pass
 
-# print(re.findall(regex, data, re.IGNORECASE))
-# try:
-# 	if len(re.findall(regex, data, re.DOTALL)[0].split(r"}")) < 2:
-# 		raise SyntaxError(r"Expected matching } at the end of sourcefiles tag")
-# 	if len(re.findall(regex, data, re.DOTALL)[0].split(r"{")) < 2:
-# 		raise SyntaxError(r"Expected matching { afrer sourcefiles tag")
-# 	if len(re.findall(regex, data, re.DOTALL)[0].split(r"}")) > 2 or len(re.findall(regex, data, re.DOTALL)[0].split(r"{")) > 2:
-# 		raise SyntaxError(r"Only a single pair of matching braces allowed")
-# except SyntaxError as what:
-# 	err = "SyntaxError: %s" % str(what)
-# 	print(err, file=sys.stderr)
-# 	sys.exit()
+    @abstractmethod
+    def build(self):
+        pass
 
-########## Core for further checks ############
-regex = r'sourceFiles\s*\{(?:\n*\s*(.*?))\}'
-match = re.findall(regex, data, re.DOTALL)
-entries = match[0].split()
-###############################################
 
-#################  Removing spaces before Line Ending ##############
+class UTE(JCSIM):
+    def __init__(self, workspace=None, products=None):
+        JCSIM.__init__(self, workspace)
+        if workspace is not None:
+            self.workspace = workspace
+        if products is not None:
+            self.protos = [each['Product'] for each in products]
+        self._LIBJARS = ""
 
-for i in xrange(len(entries)):
-	try:
-		if not entries[i].endswith(r",") and entries[i + 1] == r",":
-			entries.pop(i + 1)
-			entries[i] += r","
-		if len(entries[i].split(r".")) > 2:
-			e = "Expected , in  %s" % str(entries[i][0:-1])
-			if r"," in entries[i][0:-1] and r", " not in entries[i][0:-1]:
-				e = "Expected space after , in  %s" % str(entries[i][0:-1])
-			raise SyntaxError(e)
-	except IndexError:
-		pass
-	except SyntaxError as what:
-		err = "SyntaxError: %s" % str(what)
-		print(err, file=sys.stderr)
-		sys.exit()
-#####################################################################
+    def _make_scenario(self, proto):
+        header = os.path.join(self.workspace, 'source', 'SIMbiOS_2_9', proto, 'tst', 'config') + ';'
+        header += os.path.join(r"..", 'test') + ';'
+        header += os.path.join(r"..", 'config_fs') + ';'
+        header += os.path.join(r"..", 'applet') + ';'
+        header += os.path.join(r"..", 'src') + ';'
+        return header
 
-##################  Line-ending and empty tags verification ###############
-try:
-	if not entries:
-		raise AttributeError("No source file(s) given")
-	else:
-		for each in entries:
-			if not each.endswith(r","):
-				if each[-1].isalpha():
-					e = "Expected , after %s" % str(each)
-				else:
-					e = "Expected , instead of %s in %s" % (str(each)[-1], str(each)[0:-1])
-				# e = "Expected , after %s" % str(each)
-				raise SyntaxError(e)
-except AttributeError as what:
-	err = "AttributeError: %s" % str(what)
-	print(err, file=sys.stderr)
-except SyntaxError as what:
-	err = "SyntaxError: %s" % str(what)
-	print(err, file=sys.stderr)
-	sys.exit()
-else:
-	sources = [each.split()[0].split(r",")[0] for each in entries]
-	print(sources)
-###########################################################################
+    def _verbose(self, proto):
+        PRJ_DIR = self.workspace
+        CRD_DIR = os.path.join(PRJ_DIR, 'source', 'SIMbiOS_2_9', proto)
+        CRD_TST = os.path.join(CRD_DIR, 'tst')
+        SCDL_DIR = os.path.join(CRD_DIR, 'dev', 'sw')
+        TST_DIR = os.path.join(PRJ_DIR, 'tst')
+        UTEBAT_DIR = os.path.join(TST_DIR, 'batch')
+        MY_SCDL = os.path.join(SCDL_DIR, proto + r'.scdl')
+        MY_UTEBAT = os.path.join(UTEBAT_DIR, r'toolkit', r'Toolkit_CompleteBatch.utebat')
+        MY_LOGS = os.path.join(CRD_TST, 'proto_winpc')
+
+        print r'*******************************************************************************'
+        print '* script: %s ... starting up ...' % os.path.basename(__file__)
+        print r'*******************************************************************************'
+
+        print 'PRJ_DIR    =', PRJ_DIR
+        print 'CRD_DIR    =', CRD_DIR
+        print 'CRD_TST    =', CRD_TST
+        print 'SCDL_DIR   =', SCDL_DIR
+        print '.'
+        print 'TST_DIR    =', TST_DIR
+        print 'UTEBAT_DIR =', UTEBAT_DIR
+        print '.'
+        print 'UTEBAT     =', MY_UTEBAT
+        print 'MY_SCDL    =', MY_SCDL
+        print 'MY_LOGS    =', MY_LOGS
+        print '.'
+        print 'JAVA_HOME  ='
+        print 'CD         =', PRJ_DIR
+        # global _LIBJARS
+        self._LIBJARS = self._make_scenario(proto)
+        self._LIBJARS += ';'
+        return MY_SCDL, MY_LOGS, MY_UTEBAT
+
+    def _initiate_jcsim(self, proto=None):
+        if proto is None:
+            raise ValueError('Proto can not be None')
+        DLL_DIR = os.path.join(self.workspace, 'target', proto + r'_' + r'winPc', r'release', r'bin')
+        VJC = os.path.join(DLL_DIR, proto + r".vjc")
+        if UTE.counter > 0:
+            os.system("taskkill /f /im JCSim3x.exe")
+            UTE.counter -= 1
+        JCSim3x = subprocess.Popen("start " + self.jcsim + ' -minimized ' + " -vjc " + VJC, shell=True)
+        UTE.counter += 1
+
+    def build(self):
+        for proto in self.protos:
+            scdl, log, utebat = self._verbose(proto)
+            self.populate_libs(os.path.join(self.workspace, "tools"))
+            self.start_build(proto, scdl, utebat, log)
+
+    def start_build(self, proto, scdl, utebat, log):
+        self._initiate_jcsim(proto)
+        print '.'
+        print '+=====================================================================+'
+        print '|  CHECK PRECONDITIONS:                                               |'
+        print '|    (1)  Have you started JCSim3x.exe?                               |'
+        print '|    (2)  Is the IO-Mode adjusted in JCSim3x and UTE?                 |'
+        print '|          - e.g.: DirectIO with TLP-port:9027                        |'
+        print '|    (3)  Have you inserted a Simulation.DLL (=loaded a *.vjc file)?  |'
+        print '+=====================================================================+'
+        print '.'
+        print 'java.exe path =', os.path.join(self.workspace, 'tools', 'jdk', 'bin')
+        print 'logging.properties path =', os.path.join(self.workspace, 'source', 'SIMbiOS_2_9', proto, 'tst', 'config')
+        print '.'
+
+        print '*******************************************************************************'
+        print '* NEXT STEP: ... UTE-CONSOLE ... will be called next'
+        print '*            (com.orga.ute.gui.console.Console)'
+        print '*'
+        print '* log-files path: %s' % os.path.join(self.workspace, 'source', 'SIMbiOS_2_9', proto, 'tst',
+                                                    'proto_winpc')
+        print '*******************************************************************************'
+        print '.'
+        ############ hard coding UTEBAT file ################
+        java = os.path.join(self.workspace, 'tools', 'jdk', 'bin', 'java.exe')
+        javaOp1 = r" -d32 -Xmx384M -Xms128M -Djava.library.path="
+        cardinterfacePath = os.path.join(self.workspace, 'tools', 'cardinterface')
+        javaOp2 = r" -Djava.util.logging.config.file="
+        loggingPath = os.path.join(self.workspace, 'source', 'SIMbiOS_2_9', proto, 'tst', 'config',
+                                   'logging.properties')
+        uteConsole = r" com.orga.ute.gui.console.Console -b " + utebat + r" -PIODLL=com.orga.ute.artifact.card.reader.tlp.TlpDirectControlIO -PLOG_DIR=" + log
+        peeS = r" -Pee_software=" + scdl
+        global _LIBJARS
+        dc = dict(os.environ)
+        dc['classPath'] = self._LIBJARS
+        # print _LIBJARS
+        subprocess.Popen(java + javaOp1 + cardinterfacePath + javaOp2 + loggingPath + uteConsole + peeS, shell=True,
+                         env=dc, cwd=os.path.join(self.workspace, 'tst', 'lib'))
+
+    def populate_libs(self, tools):
+        ############## for header paths ##############
+        t = os.path.join(r"..", r"lib", r"ute", r"*") + ';'
+        self._LIBJARS += t
+        t = os.path.join(r"..", r"lib", r"*") + ';'
+        self._LIBJARS += t
+        t = os.path.join(r"..", r"..", r"source", r"uiccFramework", r"tst", r"lib") + r"\\"
+        t = os.path.join(t, r"*") + ';'
+        self._LIBJARS += t
+        ############## for core jars #################
+        libJars = []
+        for each in os.listdir(tools):
+            for e in os.listdir(os.path.join(tools, each)):
+                if e.endswith('jar'):
+                    libJars.append(os.path.join(r"..", r"..", "tools", each, e))
+
+        core = ';'.join(libJars)
+        core += ';'
+        ############## for api jars #################
+        libJars = []
+        for each in os.listdir(os.path.join(tools, 'api')):
+            for e in os.listdir(os.path.join(tools, 'api', each)):
+                if e.endswith('jar'):
+                    libJars.append(os.path.join(r"..", r"..", "tools", "api", each, e))
+        api = ';'.join(libJars)
+        api += ';'
+        ############# for xml-jdom jars ############
+        libJars = []
+        for each in os.listdir(os.path.join(tools, r'xml', r'jdom', r'1.1.1')):
+            if os.path.isdir(os.path.join(tools, r'xml', r'jdom', r'1.1.1', each)):
+                for e in os.listdir(os.path.join(tools, r"xml", r"jdom", r"1.1.1", each)):
+                    if e.endswith('jar'):
+                        libJars.append(os.path.join(r"..", r"..", "tools", "xml", "jdom", r"1.1.1", each, e))
+
+        for each in os.listdir(os.path.join(tools, r'xml', r'jdom', r'2.0.2')):
+            if os.path.isdir(os.path.join(tools, r'xml', r'jdom', r'2.0.2', each)):
+                for e in os.listdir(os.path.join(tools, r"xml", r"jdom", r"2.0.2", each)):
+                    if e.endswith('jar'):
+                        libJars.append(os.path.join(r"..", r"..", "tools", "xml", "jdom", r"2.0.2", each, e))
+
+        jdom = ";".join(libJars)
+        jdom += ";"
+        ########### for ImageConverter ############
+
+        img = os.path.join(r"..", r"..", r'tools', r'ImageConvert', r'*')
+
+        self._LIBJARS += core + api + jdom + img + ';'
+
+
+if __name__ == '__main__':
+    ute = UTE(r"D:\p4v\ssimbiOS_2_9", products)
+    ute.build()
